130011ecc82a1987e941eb9f7bd8fe6c7370a27f
ZhouSusan <szhou089@gmail.com>
2022-11-23 22:49:47 -0800

    added calculatePythagorean to PythagoreanClass

--- src/main/java/Pythagorean/PythagoreanClass.java
+++ src/main/java/Pythagorean/PythagoreanClass.java
@@ -4,8 +4,13 @@ public class PythagoreanClass {
     private double a;
     private double b;
 
-    public PythagoreanClass() {
-        this.a = a;
-        this.b = b;
+    public PythagoreanClass(double a, double b) {
+        this.a = a*a;
+        this.b = b*b;
+    }
+
+    public double calculatePythagorean() {
+        double c = Math.sqrt(this.a + this.b);
+        return c;
     }
 }
