e14d1c7f3901b14bb9f23c8cd4f6dfe1c1c3e0fd
ZhouSusan <szhou089@gmail.com>
2022-11-23 22:46:02 -0800

    added variables and constructor to PythagoreanClass

--- src/main/java/Pythagorean/PythagoreanClass.java
+++ src/main/java/Pythagorean/PythagoreanClass.java
@@ -1,4 +1,11 @@
 package Pythagorean;
 
 public class PythagoreanClass {
+    private double a;
+    private double b;
+
+    public PythagoreanClass() {
+        this.a = a;
+        this.b = b;
+    }
 }
